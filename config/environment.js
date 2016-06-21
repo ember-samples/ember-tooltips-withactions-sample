8e5481fd2df8d16a8a7db9e41a15bbaf82a73464
Miguel Madero <git@miguelmadero.com>
2016-06-21 00:49:25 -0300

    Installs CLI deploy

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.rootURL = '/ember-tooltips-withactions-sample';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
